d7b445569ca5d10d45671b731aee65073f005151
noonoo-prog <jmjbkcts@gmail.com>
2026-09-23 00:52:51 +0900

    Hide sync label outside edit mode

--- maple-weekly/boss/app.js
+++ maple-weekly/boss/app.js
@@ -996,7 +996,7 @@ function compactPartyMembers(c,bi,pi,editable){
 function compactBossCard(b,bi,c,pi,unlocked){
   var auto=!!c._sync,editable=unlocked&&!auto,mon=MONTHLY.has(b);
   var sync="";
-  if(auto){
+  if(auto&&unlocked){
     var so=c._sync.sourceOwnerName||"";
     var sp=c._sync.sourcePlayer||"";
     sync='<span class="compact-sync">↔ 자동 연동'+(so?' · '+esc(so):'')+(sp?' · '+esc(sp):'')+'</span>';
